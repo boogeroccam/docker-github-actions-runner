af7bd3d4e0558dbe4980b4a5c4f5b97a8801efb0
Ilya Bygrimov <ilya@occam.fi>
2025-07-31 14:43:28 +0200

    Prepare.sh from entrypoint.sh

--- prepare.sh
+++ prepare.sh
@@ -3,8 +3,9 @@
 echo "Checking kube dir"
 if [ ! -d $HOME/.kube ];
 then
-    "echo $HOME.kube doesnt existing, creating"
+    "echo $HOME/.kube doesnt existing, creating"
     mkdir $HOME/.kube
+    touch $HOME/.kube/config
 elif [ -d $HOME/.kube ];
 then
     echo "found /root/.kube dir"
@@ -12,4 +13,4 @@ then
     rm -rvf $HOME/.kube/*
     touch $HOME/.kube/config
 fi
-echo "$HOME.kube prepared"
+echo "$HOME/.kube prepared"
